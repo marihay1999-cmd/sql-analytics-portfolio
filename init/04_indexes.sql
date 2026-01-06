5ebfa8f42f6b06b4c12af7bb7e2dce8a8365e55c
Mari <marihay1999@gmail.com>
2026-01-06 20:28:00 +0400

    addind init files

---
+++ init/04_indexes.sql
@@ -0,0 +1,11 @@
+CREATE INDEX idx_sales_analysis_order_date
+    ON sales_analysis(order_date_date);
+
+CREATE INDEX idx_sales_analysis_year
+    ON sales_analysis(year);
+
+CREATE INDEX idx_sales_analysis_city
+    ON sales_analysis(city);
+
+CREATE INDEX idx_sales_analysis_category
+    ON sales_analysis(category);
